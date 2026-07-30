cc9d9e47a5169dad0683e89157b8d387577a9327
Giovanni Pio Grieco <giovi.5057@gmail.com>
2026-07-30 08:58:16 +0200

    Sistema di testing rompe l'include GLM... disabilitato per adesso.

--- src/ecs/systems/system.hpp
+++ src/ecs/systems/system.hpp
@@ -3,6 +3,7 @@
 namespace engine{
     class System{
         public:
+            virtual ~System() = default;
             virtual void start() = 0;
             virtual void update() = 0;
     };
